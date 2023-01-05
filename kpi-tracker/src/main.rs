999a4bb64b48dcaad53ad817f0bd74febe283bab
Søren Bruus Zeppelin <sz@concordium.com>
2023-01-05 13:56:28 +0100

    Create node client

--- kpi-tracker/src/main.rs
+++ kpi-tracker/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use concordium_rust_sdk::v2;
+use thiserror::Error;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -7,7 +8,29 @@ struct Args {
     node: v2::Endpoint,
 }
 
-fn main() {
+#[derive(Debug, Error)]
+enum NodeError {
+    /// Error establishing connection.
+    #[error("Error connecting to the node {0}.")]
+    ConnectionError(tonic::transport::Error),
+}
+
+async fn use_node(endpoint: v2::Endpoint) -> Result<(), NodeError> {
+    println!("Using node {}", endpoint.uri());
+
+    let node = v2::Client::new(endpoint)
+        .await
+        .map_err(NodeError::ConnectionError)?;
+
+    println!("{:?}", node);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
-    println!("{}", args.node.uri());
+    if let Err(error) = use_node(args.node).await {
+        println!("Error happened: {}", error)
+    }
 }
