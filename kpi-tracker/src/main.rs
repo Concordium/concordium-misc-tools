85d678fdb1e92d64a216bd271c167bf9fbddbdc3
Søren Bruus Zeppelin <sz@concordium.com>
2023-01-13 12:47:14 +0100

    Adaptions according to PR feedback

--- kpi-tracker/src/main.rs
+++ kpi-tracker/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::{BTreeMap, HashMap};
-use std::hash::Hash;
 
 use anyhow::Context;
 use chrono::{DateTime, Utc};
@@ -18,11 +17,15 @@ use futures::{self, StreamExt};
 #[derive(Debug, Parser)]
 struct Args {
     /// The node used for querying
-    #[arg(long, default_value = "http://localhost:20001")]
+    #[arg(
+        long = "node",
+        help = "The endpoint is expected to point to a concordium node grpc v2 API.",
+        default_value = "http://localhost:20001"
+    )]
     node: Endpoint,
     /// How many blocks to process.
     // Only here for testing purposes...
-    #[arg(long, default_value_t = 10000)]
+    #[arg(long = "num-blocks", default_value_t = 10000)]
     num_blocks: u64,
 }
 
@@ -36,45 +39,16 @@ struct AccountDetails {
 #[derive(Debug)]
 struct BlockDetails {
     block_time: DateTime<Utc>,
-    height: u64, // Used as a reference from where to restart on service restart.
+    height: AbsoluteBlockHeight, // Used as a reference from where to restart on service restart.
 }
 
-type AccountsTable = HashMap<CanonicalAccountAddress, AccountDetails>;
+type AccountsTable = HashMap<AccountAddress, AccountDetails>;
 type BlocksTable = HashMap<String, BlockDetails>;
 
+/// This is intended as a in-memory DB, which follows the same schema as the final DB will follow.
 struct DB {
     blocks: BlocksTable,
     accounts: AccountsTable,
-}
-
-#[derive(Eq, Debug, Clone, Copy, Ord, PartialOrd)]
-struct CanonicalAccountAddress(AccountAddress);
-
-impl From<CanonicalAccountAddress> for AccountAddress {
-    fn from(caa: CanonicalAccountAddress) -> Self {
-        caa.0
-    }
-}
-
-impl PartialEq for CanonicalAccountAddress {
-    fn eq(&self, other: &Self) -> bool {
-        let bytes_1: &[u8; 32] = self.0.as_ref();
-        let bytes_2: &[u8; 32] = other.0.as_ref();
-        bytes_1[0..29] == bytes_2[0..29]
-    }
-}
-
-impl Hash for CanonicalAccountAddress {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        let bytes: &[u8; 32] = self.0.as_ref();
-        bytes[0..29].hash(state);
-    }
-}
-
-impl AsRef<CanonicalAccountAddress> for AccountAddress {
-    fn as_ref(&self) -> &CanonicalAccountAddress {
-        unsafe { std::mem::transmute(self) }
-    }
 }
 
 /// Returns a Map of AccountAddress, AccountDetails pairs not already included in `accounts_table`
@@ -82,7 +56,7 @@ async fn new_accounts_in_block(
     node: &mut Client,
     block_hash: &HashBytes<BlockMarker>,
     accounts_table: &AccountsTable,
-) -> anyhow::Result<BTreeMap<CanonicalAccountAddress, AccountDetails>> {
+) -> anyhow::Result<BTreeMap<AccountAddress, AccountDetails>> {
     let mut accounts = node
         .get_account_list(block_hash)
         .await
@@ -92,15 +66,15 @@ async fn new_accounts_in_block(
     let mut new_accounts = BTreeMap::new();
 
     while let Some(res) = accounts.next().await {
-        let account = res.context("What exactly is this status error?")?; // TODO
-        let key = CanonicalAccountAddress(account);
+        let address = res.context("What exactly is this status error?")?; // TODO
 
-        if !accounts_table.contains_key(&key) {
-            // Client needs to be cloned for it to not be consumed on the first run.
-            let mut c_node = node.clone();
-
-            let account_info = c_node
-                .get_account_info(&AccountIdentifier::Address(account), block_hash)
+        if !accounts_table
+            .keys()
+            .into_iter()
+            .any(|stored_address| address.is_alias(stored_address))
+        {
+            let account_info = node
+                .get_account_info(&AccountIdentifier::Address(address), block_hash)
                 .await?
                 .response;
 
@@ -117,13 +91,12 @@ async fn new_accounts_in_block(
                 block_hash: block_hash.to_string(),
             };
 
-            new_accounts.insert(key, account_details);
-
             println!(
                 "NEW ACCOUNT:\naccount: {}\ndetails: {:?}",
-                &key.0,
-                new_accounts.get(&key)
+                &address, &account_details
             );
+
+            new_accounts.insert(address, account_details);
         }
     }
 
@@ -135,7 +108,7 @@ async fn new_accounts_in_block(
 fn update_db(
     db: &mut DB,
     (block_hash, block_details): (&HashBytes<BlockMarker>, BlockDetails),
-    accounts: BTreeMap<CanonicalAccountAddress, AccountDetails>,
+    accounts: BTreeMap<AccountAddress, AccountDetails>,
 ) {
     db.blocks.insert(block_hash.to_string(), block_details);
     db.accounts.extend(accounts.into_iter());
@@ -157,7 +130,7 @@ async fn handle_block(
 
     let block_details = BlockDetails {
         block_time: block_info.block_slot_time,
-        height: block_info.block_height.height,
+        height: block_info.block_height,
     };
     let new_accounts = new_accounts_in_block(node, &block_info.block_hash, &db.accounts).await?;
 
@@ -166,7 +139,7 @@ async fn handle_block(
     Ok(())
 }
 
-async fn use_node(endpoint: v2::Endpoint, height: u64) -> anyhow::Result<()> {
+async fn use_node(endpoint: v2::Endpoint, height: AbsoluteBlockHeight) -> anyhow::Result<()> {
     let args = Args::parse();
 
     println!("Using node {}\n", endpoint.uri());
@@ -181,7 +154,7 @@ async fn use_node(endpoint: v2::Endpoint, height: u64) -> anyhow::Result<()> {
         .context("Could not connect to node.")?;
 
     let mut blocks_stream = node
-        .get_finalized_blocks_from(AbsoluteBlockHeight { height })
+        .get_finalized_blocks_from(height)
         .await
         .context("Error querying blocks")?;
 
@@ -197,14 +170,14 @@ async fn use_node(endpoint: v2::Endpoint, height: u64) -> anyhow::Result<()> {
     let mut accounts: Vec<(AccountAddress, DateTime<Utc>, AccountDetails)> = db
         .accounts
         .into_iter()
-        .map(|(address_eq, details)| {
+        .map(|(address, details)| {
             let block_time = db
                 .blocks
                 .get(&details.block_hash)
                 .expect("Found account with wrong reference to block?")
                 .block_time;
 
-            (address_eq.0, block_time, details)
+            (address, block_time, details)
         })
         .collect();
 
@@ -223,7 +196,8 @@ async fn use_node(endpoint: v2::Endpoint, height: u64) -> anyhow::Result<()> {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    if let Err(error) = use_node(args.node, 0).await {
+    let height = AbsoluteBlockHeight { height: 0 };
+    if let Err(error) = use_node(args.node, height).await {
         println!("Error happened: {}", error)
     }
 }
