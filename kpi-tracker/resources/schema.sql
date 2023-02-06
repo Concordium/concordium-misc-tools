7f004652ef1957adb75d3150b2a32911ed7acf4a
Søren Bruus Zeppelin <sz@concordium.com>
2023-02-06 15:49:29 +0100

    Index on block timestamp

--- kpi-tracker/resources/schema.sql
+++ kpi-tracker/resources/schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS blocks (
   height INT8 NOT NULL,
   total_stake INT8 -- NULL means the block is NOT a payday block.
 );
+-- Create index on block timestamp to improve performance when querying for blocks within a timerange.
+CREATE INDEX IF NOT EXISTS blocks_timestamp ON blocks (timestamp);
 
 -- All accounts created.
 CREATE TABLE IF NOT EXISTS accounts (
@@ -41,11 +43,9 @@ CREATE TABLE IF NOT EXISTS transactions (
   is_success BOOLEAN NOT NULL,
   type INT2 -- NULL means the transaction was rejected
 );
-
 -- Create index on transaction type to improve performance when querying for transactions of specific types.
 CREATE INDEX IF NOT EXISTS transactions_type ON transactions USING HASH (type);
-
--- Create index on transactions reference to block to improve performance when querying for transactions linked to specific blocks. This is heavily used for account/contract activeness.
+-- Create index on transactions reference to block to improve performance when querying for transactions linked to specific blocks.
 CREATE INDEX IF NOT EXISTS transactions_block ON transactions USING HASH (block);
 
 -- Keeps track of relations between accounts and transactions to support account activeness.
