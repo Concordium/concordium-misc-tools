7da7a4288ea429762e6a723be28b89803df9c563
Søren Bruus Zeppelin <sz@concordium.com>
2023-02-10 10:00:01 +0100

    Create structure for storing activeness for performant queries

--- kpi-tracker/resources/schema.sql
+++ kpi-tracker/resources/schema.sql
@@ -46,16 +46,38 @@ CREATE TABLE IF NOT EXISTS transactions (
 -- Create index on transaction type to improve performance when querying for transactions of specific types.
 CREATE INDEX IF NOT EXISTS transactions_type ON transactions USING HASH (type);
 -- Create index on transactions reference to block to improve performance when querying for transactions linked to specific blocks.
-CREATE INDEX IF NOT EXISTS transactions_block ON transactions USING HASH (block);
+CREATE INDEX IF NOT EXISTS transactions_block ON transactions (block);
 
 -- Keeps track of relations between accounts and transactions to support account activeness.
 CREATE TABLE IF NOT EXISTS accounts_transactions (
   account INT8 NOT NULL REFERENCES accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   transaction INT8 NOT NULL REFERENCES transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+-- Create index on account-transaction relations references to transactions to improve performance when querying accounts related to specific transactions.
+CREATE INDEX IF NOT EXISTS at_transaction ON accounts_transactions (transaction);
 
 -- Keeps track of relations between contracts and transactions to support contract activeness.
 CREATE TABLE IF NOT EXISTS contracts_transactions (
   contract INT8 NOT NULL REFERENCES contracts(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   transaction INT8 NOT NULL REFERENCES transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+-- Create index on contract-transaction relations references to transactions to improve performance when querying contracts related to specific transactions.
+CREATE INDEX IF NOT EXISTS ct_transaction ON contracts_transactions (transaction);
+
+-- Create table keeping track of accounts and the dates they have been part of a transations.
+CREATE TABLE IF NOT EXISTS account_activeness (
+  account INT8 NOT NULL REFERENCES accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT, -- dependant on inserts into `accounts_transactions`.
+  time INT8 NOT NULL, -- Date represented in seconds, rounded value of the `timestamp` column on `blocks` corresponding to the block the transaction was a part of.
+  UNIQUE (account, time)
+);
+-- To support binary search instead of sequential scan of active accounts.
+CREATE INDEX IF NOT EXISTS account_activeness_time ON account_activeness (time);
+
+-- Create table keeping track of contracts and the dates they have been part of a transations.
+CREATE TABLE IF NOT EXISTS contract_activeness (
+  contract INT8 NOT NULL REFERENCES contracts(id) ON DELETE RESTRICT ON UPDATE RESTRICT, -- dependant on inserts into `contracts_transactions`.
+  time INT8 NOT NULL, -- Date represented in seconds, rounded value of the `timestamp` column on `blocks` corresponding to the block the transaction was a part of.
+  UNIQUE (contract, time)
+);
+-- To support binary search instead of sequential scan of active contracts.
+CREATE INDEX IF NOT EXISTS contract_activeness_time ON contract_activeness (time);
