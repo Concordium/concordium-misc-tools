057221f8f8732e4d28b7f41a878d6ae92801894e
Lasse Alm <lassealm2@gmail.com>
2024-07-30 10:10:02 +0200

    added validation

--- notification-server/src/google_cloud.rs
+++ notification-server/src/google_cloud.rs
@@ -3,7 +3,7 @@ use anyhow::anyhow;
 use backoff::{future::retry, ExponentialBackoff};
 use gcp_auth::{CustomServiceAccount, TokenProvider};
 use reqwest::{Client, StatusCode};
-use serde_json::json;
+use serde_json::{json, Value};
 use std::{collections::HashMap, path::PathBuf};
 
 const SCOPES: &[&str; 1] = &["https://www.googleapis.com/auth/firebase.messaging"];
@@ -38,23 +38,37 @@ impl GoogleCloud {
     }
 
     pub async fn validate_device_token(&self, device_token: &str) -> anyhow::Result<bool> {
-        let client = Client::new();
+        self.send_push_notification_with_validate(device_token, None).await
+    }
+
+    pub async fn send_push_notification(
+        &self,
+        device_token: &str,
+        information: NotificationInformation,
+    ) -> anyhow::Result<()> {
+        self.send_push_notification_with_validate(device_token, Some(information)).await.map(|_| ())
+    }
+
+    async fn send_push_notification_with_validate(&self,
+        device_token: &str,
+        information: Option<NotificationInformation>) -> anyhow::Result<bool> {
         let access_token = self.service_account.token(SCOPES).await?;
-
-        let entity_data: HashMap<String, String> = HashMap::new();
-
-        let payload = json!({
-            "message": {
-                "token": device_token,
-                "data": entity_data
-            },
-            "validate_only": true
+        let mut payload = json!({});
+        if Option::is_none(&information) {
+            payload["validate_only"] = json!(true);
+        }
+        let entity_data: HashMap<String, String> = if let Some(information) = information {
+            information.into_hashmap()
+        } else {
+            HashMap::new()
+        };
+        payload["message"] = json!({
+            "token": device_token,
+            "data": entity_data
         });
 
-        let backoff = ExponentialBackoff::default();
-
         let operation = || async {
-            let response = client
+            let response = self.client
                 .post(&self.url)
                 .bearer_auth(access_token.as_str())
                 .json(&payload)
@@ -64,13 +78,18 @@ impl GoogleCloud {
             match response {
                 Ok(res) if res.status().is_success() => Ok(true),
                 Ok(res) if res.status() == StatusCode::BAD_REQUEST => {
-                    let error_text = res.text().await.unwrap_or_default();
-                    if error_text.contains("INVALID_ARGUMENT") {
-                        Ok(false)
-                    } else {
-                        Err(backoff::Error::permanent(anyhow!(
-                            "Invalid response code returned from service: {}",
-                            error_text
+                    match res.json::<Value>().await {
+                        Ok(content) => {
+                            if content["error"]["status"] == "INVALID_ARGUMENT" {
+                                Ok(false)
+                            } else {
+                                Err(backoff::Error::permanent(anyhow!(
+                                    "Bad request sent to Google API: {}", &payload
+                                )))
+                            }
+                        },
+                        Err(err) => Err(backoff::Error::permanent(anyhow!(
+                            "Content returned from Google API is not valid json: {}", err
                         )))
                     }
                 }
@@ -79,12 +98,13 @@ impl GoogleCloud {
                     let error_text = res.text().await.unwrap_or_default();
                     if status.is_server_error() {
                         Err(backoff::Error::transient(anyhow!(
-                            "Google API responded with server error: {}",
-                            status
+                            "Google API responded with server error: {} with text {}",
+                            status,
+                            error_text
                         )))
                     } else {
                         Err(backoff::Error::permanent(anyhow!(
-                            "Invalid status code received: {} with text {}",
+                            "Google API responded with client error: {} with text {}",
                             status,
                             error_text
                         )))
@@ -93,45 +113,7 @@ impl GoogleCloud {
                 Err(e) => Err(backoff::Error::transient(anyhow!("Network error: {}", e))),
             }
         };
-
-        retry(backoff, operation).await
-    }
-
-    pub async fn send_push_notification(
-        &self,
-        device_token: &str,
-        information: NotificationInformation,
-    ) -> anyhow::Result<()> {
-        let access_token = &self.service_account.token(SCOPES).await?;
-        let entity_data: HashMap<String, String> = information.into_hashmap();
-
-        let payload = json!({
-            "message": {
-                "token": device_token,
-                "data": entity_data
-            }
-        });
-        retry(self.backoff_policy.clone(), || async {
-            let response = self
-                .client
-                .post(&self.url)
-                .bearer_auth(access_token.as_str())
-                .json(&payload)
-                .send()
-                .await;
-
-            match response {
-                Ok(res) if res.status().is_success() => Ok(()),
-                Ok(res) if res.status().is_server_error() => Err(backoff::Error::transient(
-                    anyhow!("Server error: {}", res.status()),
-                )),
-                Ok(res) => Err(backoff::Error::permanent(anyhow!(
-                    "Failed with status: {}",
-                    res.status()
-                ))),
-                Err(err) => Err(backoff::Error::transient(anyhow!("Network error: {}", err))),
-            }
-        })
-        .await
+        retry(self.backoff_policy.clone(), operation).await
     }
 }
+
