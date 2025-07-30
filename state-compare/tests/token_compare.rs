a31139d5a55259ba391d6395bf54e4af8aca4928
Richard B <richardb@Richards-MacBook-Pro.local>
2025-07-30 17:18:06 +0100

    I added types in the let lines in my test Personally, I prefer to see the types of the variables. It seems Rust is able to infer based on the usage after the let lines I am adding them to be explicitly stated so when I read I know what I am dealing with
    Jira-Id: COR-1672

--- state-compare/tests/token_compare.rs
+++ state-compare/tests/token_compare.rs
@@ -3,6 +3,7 @@ mod tests {
     use concordium_rust_sdk::protocol_level_tokens::TokenId;
     use std::{collections::HashSet, str::FromStr};
     use concordium_state_compare::token_compare::compare_token_identities;
+    use concordium_rust_sdk::types::hashes::BlockHash;
     use std::sync::Once;
 
     static INIT: Once = Once::new();
@@ -25,12 +26,12 @@ mod tests {
     async fn test_compare_token_identities_matching() {
         init_logger();
 
-        let tokens1 = vec![make_token("TokenA"), make_token("TokenB")];
-        let tokens2 = vec![make_token("TokenB"), make_token("TokenA")];
-        let block1 = [0u8; 32].into(); // Dummy BlockHash
-        let block2 = [1u8; 32].into();
+        let tokens1: Vec<TokenId> = vec![make_token("TokenA"), make_token("TokenB")];
+        let tokens2: Vec<TokenId> = vec![make_token("TokenB"), make_token("TokenA")];
+        let block1: BlockHash = [0u8; 32].into(); // Dummy BlockHash
+        let block2: BlockHash = [1u8; 32].into();
 
-        let result = compare_token_identities(&tokens1, &tokens2, block1, block2).await.unwrap();
+        let result: Vec<TokenId> = compare_token_identities(&tokens1, &tokens2, block1, block2).await.unwrap();
 
         let expected: HashSet<_> = vec![make_token("TokenA"), make_token("TokenB")].into_iter().collect();
         let result_set: HashSet<_> = result.into_iter().collect();
@@ -41,12 +42,12 @@ mod tests {
     async fn test_compare_token_identities_different() {
         init_logger();
         
-        let tokens1 = vec![make_token("TokenA")];
-        let tokens2 = vec![make_token("TokenB")];
-        let block1 = [0u8; 32].into();
-        let block2 = [1u8; 32].into();
+        let tokens1: Vec<TokenId> = vec![make_token("TokenA")];
+        let tokens2: Vec<TokenId> = vec![make_token("TokenB")];
+        let block1: BlockHash = [0u8; 32].into();
+        let block2: BlockHash = [1u8; 32].into();
 
-        let result = compare_token_identities(&tokens1, &tokens2, block1, block2).await.unwrap();
+        let result: Vec<TokenId> = compare_token_identities(&tokens1, &tokens2, block1, block2).await.unwrap();
         assert!(result.is_empty());
     }
 }
