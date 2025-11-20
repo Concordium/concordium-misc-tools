471c90550ce69a8aae05a66b6cf40f2e1b283be1
Abutalif <abutaliph@gmail.com>
2025-11-20 18:23:10 +0500

    review-changes

--- credential-verification-service/src/configs.rs
+++ credential-verification-service/src/configs.rs
@@ -6,14 +6,12 @@ use std::{net::SocketAddr, path::PathBuf};
 #[derive(Parser, Debug)]
 #[clap(arg_required_else_help(true))]
 pub struct ServiceConfigs {
-    #[arg(long, env = "CREDENTIAL_VERIFICATION_NODE_GRPC_ENDPOINT")]
-    pub node: v2::Endpoint,
-    #[arg(
-        long,
-        env = "CREDENTIAL_VERIFICATION_ADDRESS",
-        default_value = "127.0.0.1:8000"
-    )]
-    pub address: SocketAddr,
+    #[arg(long, env = "NODE_GRPC_ENDPOINT")]
+    pub node_address: v2::Endpoint,
+    #[arg(long, env = "API_ADDRESS", default_value = "127.0.0.1:8000")]
+    pub api_address: SocketAddr,
+    #[arg(long, env = "MONTITORING_ADDRESS", default_value = "127.0.0.1:8000")]
+    pub monitoring_address: SocketAddr,
     #[arg(long, help = "Path to the wallet keys.")]
     pub account: PathBuf,
     #[arg(long, default_value = "info", env = "LOG_LEVEL")]
